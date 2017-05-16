8d217b1096b19759ba197e7ebb9c527cf19d0181
krb3p <ryankatherineburke@gmail.com>
2017-05-16 08:45:53 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do | (attribute, details), hash |
+    details.each do |attribute_detail, pigeons|
+      pigeons.each do |pigeon|
+        hash[pigeon] ||= {}
+        hash[pigeon][attribute] ||= []
+        hash[pigeon][attribute] << attribute_detail.to_s
+      end
+    end
+  end
 end
+
+
+
+#   pigeons_clean = pigeons_data.flatten.uniq
+#   attributes = data.keys
+#   pigeons_clean.each do |pigeon|
+#     data.each do |attribute, attribute_detail|
+#       attribute_detail.each do |attribute_detail, names|
+#         if names.include? (pigeon)
+#           pigeons_hash[pigeon] = Hash.new
+#           pigeons_hash[pigeon][attribute] = attribute_detail
+#         end
+#       end
+#     end
+#   end
+#   binding.pry
+#   pigeons_hash
+# end
